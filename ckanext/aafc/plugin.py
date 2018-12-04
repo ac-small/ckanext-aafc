da5e936baad1600b7ce50484505f8fcbce61145a
kolesarj <john.kolesar@canada.ca>
2018-12-04 15:46:49 +0000

    Changes to id and name fields

--- ckanext/aafc/plugin.py
+++ ckanext/aafc/plugin.py
@@ -1,6 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import os.path
+from pylons.i18n import _
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultDatasetForm
+from ckan.logic import validators as logic_validators
+from routes.mapper import SubMapper
+from paste.reloader import watch_file
+#from ckantoolkit import h, chained_action, side_effect_free
+import ckanapi
+from ckan.lib.base import c
 
+from ckanext.aafc import tbs_validators
+#from ckanext.aafc import logic
+#from ckanext.aafc import auth
+from ckanext.aafc import tbs_helpers
+#from ckanext.aafc import activity as act
+#from ckanext.aafc.extendedactivity.plugins import IActivity
+
+import json
 
 class AafcPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
