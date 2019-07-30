77bc250d542e32b14de75eda4988253ed6ea31cd
jjcc <jjccforth@gmail.com>
2019-07-30 18:39:19 +0000

    dynamic dropdown function

---
+++ ckanext/aafc/fanstatic/dynamicDropdown.js
@@ -0,0 +1,37 @@
+/***
+for selection of dropdown  
+ Program Alignment Architecture to DRF Core Responsibilities
+change the option of dropdown
+ DRF Program Inventory
+
+*/
+
+
+var paa2drfData = 
+{
+ "domestic_and_international_markets":
+	['Trade and Market Expansion','Sector Engagement and Development','Farm Products Council of Canada','Dairy Programs','Canadian Pari-Mutuel Agency','Water Infrastructure','Community Pastures','Federal, Provincial and Territorial Cost-shared Markets and Trade'],
+ "domestic_and_international_markets":
+	['Foundational Science and Research','AgriScience','AgriInnovate','Agricultural Greenhouse Gases Program','Canadian Agricultural Adaptation Program','Federal, Provincial and Territorial Cost-shared Science, Research, Innovation and Environment'],
+ "science_and_innovation":
+	['AgriStability','AgriInsurance','AgriRisk','AgriInvest','AgriRecovery','Loan Guarantee Programs','Farm Debt Mediation Service','Pest Management','Assurance Program','Federal, Provincial and Territorial Cost-shared Assurance','Return of Payments'],
+ "sector_risk":
+	['Management and Oversight Services','Communication Services','Legal Services','Human Resource Management Services','Financial Management Services','Information Management Services','Information Technology Services','Real Property Services','Material Services','Acquisition Services']
+};
+
+
+$("#field-program_alignment_architecture_to_drf_core_responsibilities").change(
+	function(){
+	    $("#field-drf_program_inventory").empty();
+		var parent = $(this).children("option:selected").val();
+		var children = paa2drfData[parent];
+		children.forEach ( function(item) {
+		    var val = item.toLowerCase().replace(/\s/g,"_");
+            $("#field-drf_program_inventory").append(
+                $('<option></option>').val(val).html(item)
+            );
+		})
+	}
+);
+
+
