95dcca0b0d8fee4ad38ae6a9dc993bb32e580bb9
jjcc <jjccforth@gmail.com>
2019-12-06 14:12:18 -0500

    retrieve info from remote

--- contrib/etl/UpdateEmptyRecords.py
+++ contrib/etl/UpdateEmptyRecords.py
@@ -13,7 +13,6 @@ import os
 #ssl._create_default_https_context = ssl._create_unverified_context
 
 def get_unfilled_dataset():
-    global count
     load_dotenv()
     registry_url = os.getenv("registry_url")
     url1 = registry_url + "api/3/action/package_search"
@@ -32,9 +31,23 @@ def get_unfilled_dataset():
 
 
 
-def query_remote():
+def query_remote(package_id):
+    open_gov_url = os.getenv("open_gov_url")
+    url1 = open_gov_url + "api/3/action/package_show"
+    #q_param = "?id=b22cd297-cdb4-4d76-9f79-cc1c16d0e9e7"
+    q_param = f'?id={package_id}'
 
-    pass
+
+
+    req = urllib.request.Request(url1 + q_param, headers={'Authorization':'b5a64a5a-e737-4fe2-9ae8-f92e7c7a8072'})
+
+    response = urllib.request.urlopen(req)
+
+    res = response.read()
+    # with open("res2.json","w") as fout:
+    #     fout.write(str(res))
+    response_dict = json.loads(res)
+    return response_dict
 
 def main():
     count, res_list = get_unfilled_dataset()
@@ -43,5 +56,18 @@ def main():
         print(f'id: {a_res["id"]}, title:{a_res["title"]}')
 
 
+def syncronize_registry(package_id):
+    # English: http: // open.canada.ca / data / en / dataset / {PACKAGE_ID}
+    # French: http: // ouvert.canada.ca / data / fr / dataset / {PACKAGE_ID}
+	# metadata_modified
+    pass
+
+
+def main2():
+    query_remote()
+
+
 if __name__ == "__main__":
+    load_dotenv()
     main()
+ 
