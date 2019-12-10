394842c26c7ab6aff869549da338547f11b7af8f
jjcc <jjccforth@gmail.com>
2019-12-10 14:06:37 -0500

    AR-127: Convert to using Python 2.7

--- contrib/etl/UpdateEmptyRecords.py
+++ contrib/etl/UpdateEmptyRecords.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import ssl
 import urllib
+import urllib2
 import json
 import pprint
 from dotenv import load_dotenv
@@ -18,14 +19,16 @@ def get_unfilled_dataset():
     url1 = registry_url + "api/3/action/package_search"
     q_param = "?q=open_government_portal_record_e:n/a&fq=publication:open_government"
     # Make the HTTP request
-    response = urllib.request.urlopen(url1 + q_param)
+    #response = urllib.request.urlopen(url1 + q_param)
+    #for Python 2.7
+    response = urllib2.urlopen(url1 + q_param)
     res = response.read()
     # with open("res2.json","w") as fout:
     #     fout.write(str(res))
     response_dict = json.loads(res)
     result = response_dict['result']
     count = result['count']
-    print(f'count:{count}')
+    print("count:%d"%count)
     res_list = result['results']
     return (count, res_list)
 
@@ -35,15 +38,17 @@ def query_remote(package_id):
     open_gov_url = os.getenv("open_gov_url")
     key = os.getenv("api_key")
     url1 = open_gov_url + "api/3/action/package_show"
-    #q_param = "?id=b22cd297-cdb4-4d76-9f79-cc1c16d0e9e7"
-    q_param = f'?id={package_id}'
+    q_param = "?id=%s"%package_id
 
+    #req = urllib.request.Request(url1 + q_param, headers={'Authorization': key})
+    # for Python 2.7
+    req = urllib2.Request(url1 + q_param)
 
+    req.add_header('Authorization',key)
 
-    req = urllib.request.Request(url1 + q_param, headers={'Authorization':key})
-
-    response = urllib.request.urlopen(req)
-
+    #response = urllib.request.urlopen(req)
+    #For Python 2.7
+    response = urllib2.urlopen(req)
     res = response.read()
     # with open("res2.json","w") as fout:
     #     fout.write(str(res))
@@ -51,23 +56,71 @@ def query_remote(package_id):
     return response_dict
 
 
-def syncronize_registry(package_id):
+def syncronize_registry(package_id, data):
+    """
+    update registry data
+
+    :param package_id:
+    :param data:
+    :return:
+    """
     # English: http: // open.canada.ca / data / en / dataset / {PACKAGE_ID}
     # French: http: // ouvert.canada.ca / data / fr / dataset / {PACKAGE_ID}
     # metadata_modified
+    registry_url = os.getenv("registry_url")
+    url1 = registry_url + "api/3/action/package_show"
+    q_param = "?id=%s"%package_id
+
+    #req = urllib.request.Request(url1 + q_param) #, headers={'Authorization':key}) # Python 3.6
+    req = urllib2.Request(url1 + q_param)
+    #response = urllib.request.urlopen(req)
+    response = urllib2.urlopen(req)
+    res = response.read()
+    # with open("res2.json","w") as fout:
+    #     fout.write(str(res))
+    response_dict = json.loads(res)
+    with open("res_reg.json","w") as fout:
+        fout.write(str(res))
+
+    # dataset_dict = {
+    #     'name': 'my_dataset_name',
+    #     'notes': 'A long description of my dataset',
+    # }
+    dataset_dict = response_dict["result"]
+    #dataset_dict['id'] = response_dict["result"]["id"]
+    dataset_dict['title'] = "Open Gov test Modified"
+    dataset_dict['resources'][0]['language'] = [u'en']
+
+    #data_string = urllib.parse.quote(json.dumps(dummy))#dataset_dict))
+    data_string  = urllib.quote(json.dumps(dataset_dict))
+
+    #bdata = bytes(data_string, 'utf-8')
+    #bdata = bytes(json.dumps(dummy), 'utf-8')
+    # data = urllib.parse.urlencode(dummy)#dataset_dict)
+    # data = data.encode()
+    url1 = registry_url + "api/3/action/package_update"
+    # req = urllib.request.Request(url1, data=bdata, headers={'Authorization':'07718a69-8bed-4556-95d2-13418916cdc6',
+    #                                                                     'Content-Type':'multipart/form-data'})
+
+    request = urllib2.Request(url1)
+
+    # Creating a dataset requires an authorization header.
+    # Replace *** with your API key, from your user account on the CKAN site
+    # that you're creating the dataset on.
+    request.add_header('Authorization', '07718a69-8bed-4556-95d2-13418916cdc6')
+
+    response = urllib2.urlopen(request, data_string)
+
+    #response = urllib.request.urlopen(req)
+
+
     pass
 
 def main():
     count, res_list = get_unfilled_dataset()
     for i in range(count):
         a_res = res_list[i]
-        print(f'id: {a_res["id"]}, title:{a_res["title"]}, publication: {a_res["publication"]}')
-
-def main2():
-    res =  query_remote("xxxxxx")
-
-    data = res['result']
-    print(data['metadata_modified'])
+        print("id: %s, , title: %s, publication: %s"%( a_res["id"], a_res["title"], a_res["publication"]))
 
 if __name__ == "__main__":
     load_dotenv()
