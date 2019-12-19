892fc0078a73d06f2c44fc37ec44473ffdd01bc7
jjcc <jjccforth@gmail.com>
2019-12-19 10:36:34 -0500

    use function in another file

--- contrib/etl/UpdateEmptyRecords.py
+++ contrib/etl/UpdateEmptyRecords.py
@@ -7,6 +7,7 @@ import pprint
 from dotenv import load_dotenv
 import os
 import datetime
+from Helper import *
 
 
 # Use the json module to dump a dictionary to a string for posting.
@@ -33,36 +34,6 @@ def get_unfilled_dataset():
     print("count:%d" % count)
     res_list = result['results']
     return (count, res_list)
-
-
-def query_with_get( site, apicall, q_param, apikey = None):
-    """
-    Query remote site with get
-    :param site:
-    :param apicall:
-    :param q_param:
-    :param apikey:
-    :return:
-    """
-
-    url1 = site + apicall
-    req = urllib2.Request(url1 + q_param)
-    if apikey != None:
-        req.add_header('Authorization', apikey)
-    # response = urllib.request.urlopen(req)
-    # For Python 2.7
-    response = None
-    try:
-        response = urllib2.urlopen(req)
-    except urllib2.HTTPError:
-        # print("might not have the package in OG site yet")
-        return None
-    except:
-        print("Error happening querying OG site")
-        return None
-    res = response.read()
-
-    return res
 
 
 
