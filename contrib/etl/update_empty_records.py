d5e238531c64072bf84824ce7398c93e362fbe3e
jjcc <jjccforth@gmail.com>
2019-12-19 10:50:34 -0500

    added synchronization from OG function

--- contrib/etl/update_empty_records.py
+++ contrib/etl/update_empty_records.py
@@ -122,53 +122,9 @@ def reformat_date(date_string):
     return date_obj.strftime('%Y-%m-%d')
 
 
-def process_a_batch(data_list):
-    length = len(data_list)
-    ids = []
-    for i in range(length):
-        a_id = data_list[i]["id"]
-        ids.append(a_id)
-        #print("id:" + a_id)
-    return ids
-
-def query_by_aafc():
-    """
-    Query OG site to retrieve all the ids of AAFC
-    :return:
-    """
-    rows = 100
-    og = "https://open.canada.ca/data/"
-    apicall = "api/3/action/package_search"
-    q_param0 = "?q=organization:aafc-aac&rows=%d" % rows
-
-
-    # useful queries:
-    # ?q=metadata_modified:[2019-10-10T21:15:00Z TO *]
-    #
-    res = query_with_get(og, apicall, q_param0)
-
-    if res is None:
-        return None
-
-    response_dict = json.loads(res)
-    data = response_dict['result']
-    record_cnt = data['count']
-    results = data['results']
-    id_list = process_a_batch(results)
-    batches = record_cnt / rows +1
-
-    for b in range(1,batches):
-        offset = b * rows
-        q_param = "?q=organization:aafc-aac&rows=%d&start=%d" % (rows,offset)
-        res = query_with_get(og, apicall, q_param)
-        response_dict = json.loads(res)
-        results = response_dict['result']['results']
-        id_list_curr = process_a_batch(results)
-        id_list.extend(id_list_curr)
 
 
 
-    return id_list
 
 
 
