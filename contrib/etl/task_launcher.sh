12b2eb6cadc002064ad33d7b7bbc90bba6175d12
Alexis S <alexis.small@canada.ca>
2020-01-30 10:57:29 -0500

    added task launcher

---
+++ contrib/etl/task_launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to run scheduled task scripts
+# Update using appropriate server, port, mail sender and recipient
+
+cd /home/rootadmin/docker/projects/ckanext-aafc/contrib/etl
+
+python publish_to_og.py
+python synch_with_og.py
+python update_empty_records.py
+python send_email.py --server --port --from --to 
