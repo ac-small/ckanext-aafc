86f7ee139931fed5efa8c6b475ad631312d33197
Alexis S <alexis.small@canada.ca>
2020-02-19 09:22:35 -0500

    keep group value when updating

--- contrib/etl/synch_with_og.py
+++ contrib/etl/synch_with_og.py
@@ -173,7 +173,8 @@ def update_to_registry(package_id):
                 'publication': reg_data['publication'],
                 'data_released': reg_data['data_released'],
                 'open_government_portal_record_e': reg_data['open_government_portal_record_e'],
-                'open_government_portal_record_f': reg_data['open_government_portal_record_f']
+                'open_government_portal_record_f': reg_data['open_government_portal_record_f'],
+                'groups': reg_data['groups']
             }
             # Keep ODI number if it exists, some datasets may not have an ODI.
             if 'odi_reference_number' in reg_data:
