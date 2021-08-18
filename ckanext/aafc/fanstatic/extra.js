bea6e99c05c07f043aae4384bfa9a8238fa40040
alexis s <alexis.small@canada.ca>
2021-08-18 12:56:20 +0000

    AR-172: Add clear button to search bar

--- ckanext/aafc/fanstatic/extra.js
+++ ckanext/aafc/fanstatic/extra.js
@@ -2,4 +2,9 @@
 place holder for future use
 */
 
+// Reset Function to Clear Search Box
+function resetFunction() {
+        document.getElementById("field-giant-search").value = "";
+        document.getElementById("search-btn").click();
+}
 
