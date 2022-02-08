00423dbb34d730f2a24d5dc3f6e79c28155736a1
jjcc <jjccforth@gmail.com>
2022-02-07 23:14:26 -0500

    script fix

--- contrib/docker-dev29/appInstall.sh
+++ contrib/docker-dev29/appInstall.sh
@@ -4,8 +4,8 @@ INI_FILE=/app/ckan/config/dev.ini
 . /app/ckan/venv/bin/activate; cd /app/ckan/venv/src/
 
 # git clone -b python3 https://github.com/asc-csa/ckanext-asc-csa-scheming && git clone https://github.com/ckan/ckanext-fluent.git && git clone https://github.com/asc-csa/ckanext-asc-csa
-git clone -b v2_9-compat https://github.com/aafc-ckan/ckanext-aafc && pushd ckanext-aafc && python setup.py develop && popd
-git clone -b v2_9 https://gitlab.com/aafc/ckanext-scheming.git && pushd ckanext-scheming && python setup.py develop && popd
+git clone -b v2_9 https://github.com/aafc-ckan/ckanext-aafc && pushd ckanext-aafc && python setup.py develop && popd
+git clone -b v2_9 https://github.com/aafc-ckan/ckanext-scheming.git && pushd ckanext-scheming && python setup.py develop && popd
 
 sed -i '/ckan.plugins/ s/$/ aafc/' $INI_FILE
 sed -i '/ckan.plugins/ s/$/ scheming_datasets/' $INI_FILE
