56a4441bf1ad741494d791a306f2ebb94f4081d9
jjcc <jjccforth@gmail.com>
2023-02-20 14:32:39 -0500

    bug fix

--- contrib/etl/synch_with_og.py
+++ contrib/etl/synch_with_og.py
@@ -9,6 +9,7 @@ from ckanapi import RemoteCKAN
 
 to_replace = {"owner_org": "d266f358-17ff-4625-b126-da390146909c" , "org_title_at_publication":"agriculture_and_agrifood_canada"  }
 
+aafc_org_id = "2ABCCA59-6C57-4886-99E7-85EC6C719218"
 
 def process_a_batch(data_list):
     length = len(data_list)
@@ -41,7 +42,7 @@ def query_site_for_newdata(site, sec_param="", hours_ago=None):
         hours_ago = 48
     two_days_ago = datetime.now() - timedelta(hours=hours_ago)
     str_2days_ago =  two_days_ago.strftime('%Y-%m-%dT%H:%M:%SZ')
-    q_param1 = "?q=metadata_modified:[%s%sTO%s*]"%(str_2days_ago, '%20','%20') + sec_param
+    q_param1 = "?q=owner_org:%s%sAND%smetadata_modified:[%s%sTO%s*]"%(aafc_org_id, '%20', '%20',str_2days_ago, '%20','%20') + sec_param
     res = query_with_get(site, apicall, q_param1)
 
     if res is None:
@@ -201,6 +202,8 @@ def extract_branch_and_data_steward(og_data):
         contact_str = pattern.split(contact)
     elif 'org_section' in og_data and og_data['org_section']['en'] != {}:
         branch = og_data['org_section']['en']
+        if len(branch)  == 0:
+            branch = "Unknown Branch"
         if 'maintainer_email' in og_data and og_data['maintainer_email'] != {}:
             data_steward = og_data['maintainer_email']
         else:
